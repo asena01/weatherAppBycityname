415f2063e6934c4508719450f7add58de0c69518
asena dawit <dawit@asena.local>
2021-08-08 18:47:57 +0300

    Heroku deploy v3

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 app.use(sslRedirect());
-app.use(express.static(__dirname + '/dist/weather-app'));
+app.use(express.static(__dirname + '/dist/weatherApp'));
 app.listen(process.env.PORT || 5000);
 
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/weatherApp/index.html'))
 });
