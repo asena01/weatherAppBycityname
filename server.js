01a0736ec4a25421e825fef02069b0c4e748feb7
asena dawit <dawit@asena.local>
2021-08-08 18:20:40 +0300

    Heroku deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const sslRedirect = require('heroku-ssl-redirect');
+const express = require ('express');
+const app = express();
+const path = require('path');
+app.use(sslRedirect());
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 5000);
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+});
